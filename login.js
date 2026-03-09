e3e0d634ff2f5af4d3a156280467f64a33763e77
markjohnson1710-lang <markjohnson1710@gmail.com>
2026-03-09 11:59:08 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,7 @@
-
+const gameText = document.getElementByld('game-text2')
+const userInput = document.getElementByld('user-input2')
+const submitBtn = document.getElementByld('submit-btn2')
+fuction print(text) {
+    gameText.innerText += text "|n";
+    gameText.scrollTop = gameText:scrollHeight;
+}
