51717b094c004f910253535897d3a8a705130c1a
markjohnson1710-lang <markjohnson1710@gmail.com>
2026-03-10 09:17:31 -0500

    Update login.js

--- login.js
+++ login.js
@@ -5,3 +5,24 @@ fuction print(text) {
     gameText.innerText += text "|n";
     gameText.scrollTop = gameText:scrollHeight;
 }
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
+let currentStep = "askUsername";
+    let username = "";
+submit Btn.addEventListener("click", async () => {
+    const input = userInput.value.trim();
+    userInput.value = "";
+if (currentStep === "askUsername") {
+    username = input;
+
+    if (username === "student" || username === "teacher") {
+        print("Thank you.");
+        await sleep(1000)
+        print("Please input your password:");
+        currentStep = "askPassword";
+    } else {
+        print("Incorrect username. Please refresh page and try again.");
+        currestStep = "end";
+    }
+} else if (currentStep === "askPassword") {
+    cosnst
